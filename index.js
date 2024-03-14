22301e51b2a2c764080fcd74f812f6d6968677d8
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-03-14 11:50:25 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+// Define the cats array with initial values
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Define the array functions
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+// Export the necessary variables and functions
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat
+};
